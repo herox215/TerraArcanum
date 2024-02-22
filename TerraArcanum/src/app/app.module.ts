76fc82ebf5b1a46f1cb8264950d8ae33fd01cecf
Lucas Henze <henze94@outlook.com>
2024-02-22 17:46:47 +0100

    Added basic lets get started implementation

---
+++ TerraArcanum/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MapDisplayComponent } from './map-display/map-display.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapDisplayComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ TerraArcanum/src/app/map-display/map-display.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map-display',
+  templateUrl: './map-display.component.html',
+  styleUrls: ['./map-display.component.scss']
+})
+export class MapDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+  }
+}

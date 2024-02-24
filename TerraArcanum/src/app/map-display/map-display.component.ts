2465e0e37ae14196e7ddc60a79f7e87138d164da
Lucas <henze94@outlook.com>
2024-02-24 10:32:45 +0100

    Added Map png and changed component so it loads the map

--- TerraArcanum/src/app/map-display/map-display.component.ts
+++ TerraArcanum/src/app/map-display/map-display.component.ts
@@ -11,11 +11,26 @@ export class MapDisplayComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    // Initialisieren der Karte
+    const map = L.map('map', {
+      center: [51.505, -0.09], // Dies sollte auf den Mittelpunkt Ihres Bildes angepasst werden
+      zoom: 13
+    });
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    // Optional: Hinzufügen einer minimalen oder transparenten Basiskarte
+    L.tileLayer('', {
+      attribution: 'Your Attribution Here'
+    }).addTo(map);
+
+    // Pfad zu Ihrem Bild
+    const imageUrl = './assets/map.png';
+    // Geografische Koordinaten der Ecken Ihres Bildes
+    const imageBounds: L.LatLngBoundsExpression = [
+      [51.49, -0.08], // Untere linke Ecke
+      [51.5, -0.06]  // Obere rechte Ecke
+    ];
+
+    // Hinzufügen Ihres Bildes als Overlay
+    L.imageOverlay(imageUrl, imageBounds).addTo(map);
   }
 }
